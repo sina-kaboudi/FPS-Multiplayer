3a07703ac53300598144a7330863cd9efcaa0339
sina-kaboudi <74722028+sina-kaboudi@users.noreply.github.com>
2021-08-11 13:53:11 +0430

    Implement Section 2
    The player can now jump and run.
    Minor fixes to cursor mode on build.

--- FPS-Multiplayer/Assets/Scripts/PlayerController.cs
+++ FPS-Multiplayer/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform viewPoint;
     [SerializeField] float mouseSensetivity = 1f;
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float walkSpeed = 5f, runSpeed = 8f;
     [SerializeField] CharacterController characterController;
+    [SerializeField] float jumpForce = 12f, gravityModifier = 2.5f;
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] LayerMask groundLayers;
 
     float verticleRotStore;
     Vector2 mouseInput;
-    Vector3 moveInput, moveDirection;
+    Vector3 moveInput, movement;
+    float currentMoveSpeed;
+    Camera cam;
+    bool isGrounded;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
 
     private void Start()
     {
@@ -35,9 +46,52 @@ public class PlayerController : MonoBehaviour
             viewPoint.rotation.eulerAngles.y,
             viewPoint.rotation.eulerAngles.z);
 
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveDirection = ((transform.forward * moveInput.z) + (transform.right * moveInput.x)).normalized;
 
-        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentMoveSpeed = runSpeed;
+        }
+        else
+        {
+            currentMoveSpeed = walkSpeed;
+        }
+
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        float yVelocity = movement.y;
+        movement = ((transform.forward * moveInput.z) + (transform.right * moveInput.x)).normalized * currentMoveSpeed;
+        movement.y = yVelocity;
+        if (characterController.isGrounded)
+        {
+            movement.y = 0;
+        }
+
+        isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, 0.35f, groundLayers);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            movement.y = jumpForce;
+        }
+
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityModifier;
+
+        characterController.Move(movement * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Cursor.lockState == CursorLockMode.None)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.transform.position;
+        cam.transform.rotation = viewPoint.transform.rotation;
     }
 }
